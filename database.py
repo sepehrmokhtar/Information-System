c4e0140232fabcccd36732c462676f144cb6751e
Sina Najafi <148863702+SinaNajafi1@users.noreply.github.com>
2024-11-22 20:00:45 +0100

    Update database.py

--- database.py
+++ database.py
@@ -3,34 +3,44 @@ import mysql.connector
 # Establish a connection to the MySQL database
 connection = mysql.connector.connect(
     host='sql7.freesqldatabase.com',       # MySQL host
-    user='sql7741129',   # MySQL username
-    password='XUQ35MWg9D',  # MySQL password
-    database='sql7741129'   # Connect to the database
+    user='sql7746745',   # MySQL username
+    password='qVMtw78yWQ',  # MySQL password
+    database='sql7746745'   # Connect to the database
 )
 
 # Create a cursor object
 cursor = connection.cursor()
 
-# Write SQL queries to create the tables. TODO: largen the password capacity to be avle to store large hashed passwords.
+# Write SQL queries to create the tables
 Doctor = """
 CREATE TABLE doctors (
     doctor_id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    specialization VARCHAR(100),
-    email VARCHAR(100),
-    password VARCHAR(15),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    specialization VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    organization VARCHAR(100) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(200) NOT NULL,
     PRIMARY KEY (doctor_id)
 );
 """
 
 Patient = """
 CREATE TABLE patients (
-    patient_id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    email VARCHAR(100),
-    address VARCHAR(100)
+    patient_id INT NOT NULL AUTO_INCREMENT, 
+    first_name VARCHAR(100) NOT NULL,          
+    last_name VARCHAR(100) NOT NULL,           
+    email VARCHAR(100) NOT NULL UNIQUE,        
+    address VARCHAR(100) NOT NULL,             
+    gender VARCHAR(100) NOT NULL,              
+    age INT NOT NULL,                          
+    race VARCHAR(100) NOT NULL,               
+    insurance_number CHAR(20) NOT NULL,        
+    phone_number VARCHAR(100) NOT NULL,        
+    admission_date DATE NOT NULL,              
+    responsible_doctor VARCHAR(100) NOT NULL,
     PRIMARY KEY (patient_id)
 );
 """
